0e505b3522ebec0cc8b44d85f28490b6a0bf0bbc
Lohan Pinheiro <o.lohan.yrvine@gmail.com>
2021-12-04 22:11:22 -0300

    arrumando arquivo depois do primeiro testo

--- script.sh
+++ script.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
 
 # ----------------------------- VARIÁVEIS ----------------------------- #
-PPA_LIBRATBAG="ppa:libratbag-piper/piper-libratbag-git"
-PPA_CHROMIUM="ppa:xalt7x/chromium-deb-vaapi"
-
-URL_DISCORD="https://discord.com/api/download?platform=linux&format=deb"
-
 DOWNLOAD_FOLDER="$HOME/Downloads/programs"
 
+PPA_REPOSITORIOS=(
+    ppa:libratbag-piper/piper-libratbag-git
+    ppa:xalt7x/chromium-deb-vaapi
+)
+
 GITHUB_REPOSITORIOS=(
-    https://github.com/Lohan-Yrvine/config-files
     https://github.com/alacritty/alacritty.git
     https://github.com/Lohan-Yrvine/wallpapers
+)
+
+PROGRAMAS_DEB=(
+
 )
 
 PROGRAMAS_APT=(
     snapd
     flatpak
-    flameshot
-    htop
-    bat
     neofetch
-    neovim
     ratbagd
     piper
     chromium-browser
@@ -29,28 +28,40 @@ PROGRAMAS_APT=(
     yarn
     nodejs
     zsh
-    cmake
     pkg-config
     libfreetype6-dev
     libfontconfig1-dev
     libxcb-xfixes0-dev
     libxkbcommon-dev
     python3
-    fonst-firacode
+    fonts-firacode
     exuberant-ctags
-    ccls
-    vlc
     timeshift
     curl
     wallch
     tmux
 )
 
+PROGRAMAS_SNAP=(
+    spotify
+    discord
+    nvim
+    flameshot
+    htop
+    cmake
+    ccls
+    vlc
+    curl
+)
+
+PROGRAMAS_FLATPAK=(
+
+)
+
 PROGRAMAS_CURL=(
     "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs
 )
 
 # ----------------------------- REQUISITOS ----------------------------- #
@@ -59,10 +70,11 @@ sudo rm /var/lib/dpkg/lock
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 
-sudo apt update
+sudo apt update -y
 
-sudo apt-add-repository "$PPA_LIBRATBAG" -y
-sudo apt-add-repository "$PPA_CHROMIUM" -y
+for repositorio in ${PPA_REPOSITORIOS}; do
+    sudo apt-add-repository $repositorio -y
+done
 sudo apt-add-repository universe
 
 for repositorio in ${GITHUB_REPOSITORIOS}; do
@@ -70,36 +82,38 @@ for repositorio in ${GITHUB_REPOSITORIOS}; do
 done
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
-sudo apt update
+sudo apt update -y
 
-mkdir "$DOWNLOAD_FOLDER" 
-wget -c "$URL_DISCORD" -P "$DOWNLOAD_FOLDER"
-
+mkdir $DOWNLOAD_FOLDER 
+for programa in ${PROGRAMAS_DEB}; do
+    wget -c $programa -p $DOWNLOAD_FOLDER
+done
 sudo dpkg -i $DOWNLOAD_FOLDER/*.deb
 
 for programa in ${PROGRAMAS_APT}; do
-    if ! dpkg -l | grep -q $programa; then
-        sudo apt install $programa -y
-    else
-        echo "[INSTALADO] - $programa"
-    fi
+    sudo apt install $programa -y
+done
+
+for programa in ${PROGRAMAS_SNAP}; do
+    sudo snap install $programa
+done
+
+for programa in ${PROGRAMAS_FLATPAK}; do
+    sudo flatpak install $programa
 done
 
 for programa in ${PROGRAMAS_CURL}; do
     sh -c $programa
 done
 
-# zsh plugins
+## zsh plugins ##
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+## Rust ##
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup override set stable
 rustup update stable
-
-## Pacotes Flakpak ##
-
-## Pacotes Snap ##
-sudo snap install spotify
 
 ## Alacritty ##
 cd alacritty
@@ -122,8 +136,9 @@ gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-m
 cd
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
-sudo apt update
+sudo apt update -y
 sudo apt dist-upgrade
+sudo snap refresh
 flatpak update
 sudo apt autoclean
 sudo apt autoremove -y
@@ -140,6 +155,7 @@ cp -f ~/config-files/alacritty.yml ~/.config/alacritty
 cp -f ~/config-files/.zshrc ~/
 cp -f ~/config-files/.tmux.conf ~/
 
+echo ""
 echo "---------- LISTA DE AJUSTES MANUAIS QUE PRECISAM SER FEITOS ----------"
 echo "setar o flameshot para iniciar com o SO"
 echo "mudar o PrtSc para printar com o flameshot por padrão (flameshot gui)"
