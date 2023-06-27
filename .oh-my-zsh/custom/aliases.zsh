65fdb63f5731f68fc72b6fd2387b7a2f7062ed25
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2023-06-27 19:14:46 -0300

    fixing omz configs

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,25 @@
+alias v="nvim"
+
+alias ls="exa"
+alias ll="exa -l"
+
+alias cat="batcat"
+
+alias gs="git status"
+
+alias upgrade="
+echo \"SUDO APT UPDATE\" &&
+sudo apt update &&
+echo \"\nSUDO APT UPGRADE\" &&
+sudo apt upgrade -y &&
+echo \"\nSUDO APT AUTOREMOVE\" &&
+sudo apt autoremove -y &&
+echo \"\nSUDO APT AUTOCLEAN\" &&
+sudo apt autoclean &&
+echo \"\nFLATPAK UPDATE\" &&
+flatpak update -y &&
+echo \"\nFLATPAK REMOVE UNUSED\" &&
+flatpak remove --unused -y &&
+echo \"\nSNAP REFRESH\" &&
+sudo snap refresh
+"
