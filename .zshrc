e23a7b4cb1dabc08e4860bff765b76e7f8359e2f
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2023-12-13 13:07:41 -0300

    catppuccin theme

--- .zshrc
+++ .zshrc
@@ -11,20 +11,3 @@ plugins=(git zsh-autosuggestions zsh-syntax-highlighting tmux)
 ZSH_TMUX_AUTOSTART=true
 
 source $ZSH/oh-my-zsh.sh
-
-# User configuration
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/lohanyrvine/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/lohanyrvine/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/lohanyrvine/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/lohanyrvine/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
--- aliases.zsh
+++ aliases.zsh
@@ -1,12 +1,14 @@
-alias v="nvim"
-
 function nvrc_func() {
   SAVE_PWD=($PWD)
   cd ~/.config/nvim && nvim . && cd $SAVE_PWD
 }
 alias nvrc=nvrc_func
+alias v="nvim"
 
 alias aliases="nvim ~/.oh-my-zsh/custom/aliases.zsh && source ~/.oh-my-zsh/custom/aliases.zsh"
+alias zshrc="nvim ~/.zshrc && source ~/.zshrc"
+alias tmuxrc="nvim ~/.tmux.conf && tmux source ~/.tmux.conf"
+alias alacrittyrc="nvim ~/.alacritty.yml"
 
 alias ls="exa"
 alias ll="exa -l --group-directories-first"
@@ -15,21 +17,28 @@ alias cat="batcat --theme=gruvbox-light"
 
 alias mark="grip -b"
 
-alias upgrade="
-echo \"SUDO APT UPDATE\" &&
-sudo apt update &&
-echo \"\nSUDO APT UPGRADE\" &&
-sudo apt upgrade -y &&
-echo \"\nSUDO APT AUTOREMOVE\" &&
-sudo apt autoremove -y &&
-echo \"\nSUDO APT AUTOCLEAN\" &&
-sudo apt autoclean &&
-echo \"\nFLATPAK UPDATE\" &&
-flatpak update -y &&
-echo \"\nFLATPAK REMOVE UNUSED\" &&
-flatpak remove --unused -y &&
-echo \"\nSNAP REFRESH\" &&
-sudo snap refresh
-"
+function upgrade_func() {
+  echo "SUDO APT UPDATE"
+  sudo apt update
+
+  echo "\nSUDO APT UPGRADE"
+  sudo apt upgrade -y
+
+  echo "\nSUDO APT AUTOREMOVE"
+  sudo apt autoremove -y
+
+  echo "\nSUDO APT AUTOCLEAN"
+  sudo apt autoclean
+
+  echo "\nFLATPAK UPDATE"
+  flatpak update -y
+
+  echo "\nFLATPAK REMOVE UNUSED"
+  flatpak remove --unused -y
+
+  echo "\nSNAP REFRESH"
+  sudo snap refresh
+}
+alias upgrade=upgrade_func
 
 alias cubemx="/usr/local/STMicroelectronics/STM32Cube/STM32CubeMX/STM32CubeMX"
