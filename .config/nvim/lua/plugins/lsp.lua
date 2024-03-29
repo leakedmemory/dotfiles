29440f7d71cc4bdb0c82e187e6a887bbd176e443
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-03-29 14:52:37 -0300

    nvim: use local clangd as lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,14 +14,15 @@ return {
   },
   name = "lsp",
   config = function()
-    local lsp_zero = require("lsp-zero")
+    local lsp = require("lsp-zero")
+    lsp.preset("recommended")
 
-    lsp_zero.on_attach(function(_, bufnr)
+    lsp.on_attach(function(_, bufnr)
       -- availables actions in `:help lsp-zero-keybindings`
-      lsp_zero.default_keymaps({ buffer = bufnr })
+      lsp.default_keymaps({ buffer = bufnr })
     end)
 
-    lsp_zero.format_on_save({
+    lsp.format_on_save({
       format_opts = {
         async = false,
         timeout_ms = 10000,
@@ -29,7 +30,7 @@ return {
       servers = {
         ["lua_ls"] = { "lua" },
         ["vimls"] = { "vim" },
-        ["clangd"] = { "c", "cpp" },
+        ["clangd"] = { "c", "cpp", "objc", "objcpp" },
         ["rust_analyzer"] = { "rust" },
         ["taplo"] = { "toml" },
         ["yamlls"] = { "yaml" },
@@ -42,19 +43,18 @@ return {
       ensure_installed = {
         "lua_ls",
         "vimls",
-        "clangd",
         "rust_analyzer",
         "taplo",
         "yamlls",
         "pyright",
       },
       handlers = {
-        lsp_zero.default_setup,
+        lsp.default_setup,
       },
     })
 
     local cmp = require("cmp")
-    local cmp_action = lsp_zero.cmp_action()
+    local cmp_action = lsp.cmp_action()
 
     cmp.setup({
       mapping = cmp.mapping.preset.insert({
@@ -72,6 +72,14 @@ return {
 
     local lspconfig = require("lspconfig")
 
+    lspconfig.opts = {
+      servers = {
+        clangd = {
+          mason = false,
+        },
+      },
+    }
+
     lspconfig.lua_ls.setup({
       settings = {
         Lua = {
@@ -79,6 +87,16 @@ return {
             globals = { "vim" },
           },
         },
+      },
+    })
+
+    lspconfig.clangd.setup({
+      cmd = {
+        "clangd-18",
+        "--background-index",
+        "--suggest-missing-includes",
+        "--clang-tidy",
+        "--header-insertion=iwyu"
       },
     })
   end,
