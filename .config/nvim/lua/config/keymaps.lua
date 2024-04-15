b8669efbb52500b996a1607ef811f8f2ef7de453
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-04-15 09:53:04 -0300

    nvim: moving from `vim.api.nvim_set_keymap` to `vim.keymap.set`

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,26 +6,18 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- mantain selection of text after using < and >
-vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true })
-vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true })
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
 
 -- move the current line up and down
-vim.api.nvim_set_keymap("n", "<C-k>", ":move .-2<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-k>", ":move .-2<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-j>", ":move .+1<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-j>", ":move .+1<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-k>", ":move .-2<CR>")
+vim.keymap.set({ "n", "v" }, "<C-j>", ":move .+1<CR>")
 
 -- delete to the void
-vim.keymap.set("n", "<leader>d", '"_d')
-vim.keymap.set("v", "<leader>d", '"_d')
-
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
 -- delete to the void and paste
 vim.keymap.set("x", "<leader>p", '"_dP')
 
 -- copy to the SO clipboard
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
-
--- replace everything
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
