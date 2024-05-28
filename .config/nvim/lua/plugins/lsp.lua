712db616469203f2d483492115dd3e437911f0e8
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-05-28 09:59:55 -0300

    nvim: new keymaps

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -97,7 +97,6 @@ return {
           diagnostics = {
             globals = {
               "vim",
-              "xplr",
             },
           },
         },
--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -1,7 +1,23 @@
 local set = vim.keymap.set
 
+set("n", "x", '"_x')
+
+set("n", "U", "<C-r>")
+
+-- open link under cursor on browser
+set("n", "gx", ":sil !open <cWORD><cr>", { silent = true })
+
+-- center buffer while navigating
 set("n", "<C-d>", "<C-d>zz")
 set("n", "<C-u>", "<C-u>zz")
+set("n", "{", "{zz")
+set("n", "}", "}zz")
+set("n", "N", "Nzz")
+set("n", "n", "nzz")
+set("n", "G", "Gzz")
+set("n", "%", "%zz")
+set("n", "*", "*zz")
+set("n", "#", "#zz")
 
 -- toggle fold, open all folds, close all folds
 set({ "n", "v" }, "<leader>ft", "za")
@@ -23,6 +39,5 @@ set({ "n", "v" }, "<leader>d", '"_d')
 -- delete to the void and paste
 set("x", "<leader>p", '"_dP')
 
--- copy to the SO clipboard
+-- copy to the OS's clipboard
 set({ "n", "v" }, "<leader>y", '"+y')
-set("n", "<leader>Y", '"+Y')
