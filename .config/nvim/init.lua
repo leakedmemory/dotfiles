14e494d011cceb54cffa365c07273ddd49d01a07
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-06-29 18:19:44 -0300

    nvim: adding make and jsonc ftplugin config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,9 @@
 vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+
+---@diagnostic disable-next-line: undefined-field
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
